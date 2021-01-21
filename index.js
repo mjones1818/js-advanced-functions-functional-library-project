858a757ef82e81ce00800ffd1965f1e5cb78398c
mjones1818 <michael.jones0532@gmail.com>
2021-01-21 12:32:58 -0600

    commit tree dd05c77df10b1e03dca3eed08630b02997518ae9

--- index.js
+++ index.js
@@ -4,16 +4,40 @@ const fi = (function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
 
-    each: function() {
-
+    each: function(collection, callback) {
+      let newCollection = (Array.isArray(collection)) ? collection.slice() : Object.values(collection)
+      for (let i =0; i <newCollection.length; i++) {
+        callback(newCollection[i])
+      }
+      
+      return collection
     },
 
-    map: function() {
-
+    map: function(collection, callback) {
+      let results = []
+      if (!Array.isArray(collection)) {
+        collection = Object.values(collection)
+      }
+      for (let i = 0; i < collection.length; i++) {
+        results.push(callback(collection[i]))
+      } 
+      return results
     },
 
-    reduce: function() {
+    reduce: function(c = [], callback = () => {}, acc) {
+      let collection = c.slice(0)
 
+			if (!acc) {
+				acc = collection[0]
+				collection = collection.slice(1)
+			}
+
+			let len = collection.length;
+
+			for (let i = 0; i < len; i++) {
+				acc = callback(acc, collection[i], collection)
+			}
+			return acc;
     },
 
     functions: function() {
@@ -25,3 +49,6 @@ const fi = (function() {
 })()
 
 fi.libraryMethod()
+
+
+let test = 9
